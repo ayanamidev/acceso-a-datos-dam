9c20d44a5afaf05511a2980a52d294fef2c5ed3f
LauraDev <lauralodeirodev@gmail.com>
2024-09-24 09:37:53 +0200

    Cambios

---
+++ nioII.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class nio {
+    public static void main(String[] args) {
+        try{
+            Path origen=Paths.get("C:\\Users\\laura\\Desktop\\pruebaNio.txt");
+            BufferedReader br=Files.newBufferedReader(origen);
+            Stream<String> lineas=br.lines();
+            lineas.forEach(System.out::println);
+            br.close();   
+
+        }catch ( IOException e) {
+            
+            System.out.println("ERROR");
+
+        }
+
+        
+    }
+}
